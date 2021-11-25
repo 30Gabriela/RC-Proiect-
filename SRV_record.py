e09579a3eb453dfd0ffefdf0a566b73406b9bf47
Gabriela Brezeanu <91419044+30Gabriela@users.noreply.github.com>
2021-11-25 16:04:03 +0200

    Init SRV class

---
+++ SRV_record.py
@@ -0,0 +1,11 @@
+class SRV_record:
+    def __init__(self,name_service, protocol, domain_name, ttl, priority, weigth, port, target):
+        self.name_service=name_service
+        self.protocol=protocol  #mereu e TCP/IP
+        self.domain_name=domain_name #numele de domeniu unde aceasta intrare este valida
+        self.ttl=ttl   #time to alive
+        self.priority=priority  #prioritatea gazdei tinta
+        self.weight=weigth  #o valoare relativa pentru intrarile cu aceeasi greutate
+        self.port=port      #portul TCP unde va fi gasit serviciul
+        self.target=target #numele de gazda al dispozitivului ce pune la dispozitie acel serviciu
+
