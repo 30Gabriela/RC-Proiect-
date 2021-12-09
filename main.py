0b0cf1117e912baa8f430b7e2ea1436a09641e4c
Gabriela Brezeanu <91419044+30Gabriela@users.noreply.github.com>
2021-12-09 20:23:37 +0200

    interfata are thread separat

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import Local_network
 import Server
 import Responder
+import threading
 
 if __name__=='__main__':
-    Responder.startInterface()
     MainServer = Server.UDP()
     # se creeaza reteaua locala
     local_network = Local_network.Local(MainServer)
     # setam in server reteaua locala
     MainServer.set_local_network(local_network)
     MainServer.start()
+    interface_thread = threading.Thread(Responder.startInterface(MainServer))
+    interface_thread.start()
