b15d8ecc9f70bccb6024d9864beb4882e21ea715
Gabriela Brezeanu <91419044+30Gabriela@users.noreply.github.com>
2021-12-09 18:49:41 +0200

    functie start interface+init server/local network

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from PyQt5 import QtWidgets
-from Responder import startInterface,Ui_Responder
-from Resolver import Ui_MainWindow
+import Local_network
+import Server
+import Responder
+
 if __name__=='__main__':
-    startInterface()
+    Responder.startInterface()
+    MainServer = Server.UDP()
+    # se creeaza reteaua locala
+    local_network = Local_network.Local(MainServer)
+    # setam in server reteaua locala
+    MainServer.set_local_network(local_network)
+    MainServer.start()
