9f0db2816513985c6820989a07fc0de475e3ab31
Gabriela Brezeanu <91419044+30Gabriela@users.noreply.github.com>
2021-12-09 20:23:10 +0200

    receive demo

--- Server.py
+++ Server.py
@@ -15,48 +15,54 @@ class UDP:
         try:
             # Creare socket UDP
             self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            # Asociere la adresa locala, portul 5000
             self.server.bind(('127.0.0.1', 20001))
             print("UDP server up and listening")
         except BaseException as error:
             print("Error " + str(error))
             raise
 
-        #vom avea nevoie de un thread pentru asteptarea noilor conexiuni
-        #self.ThreadForNewConnections = threading.Thread(target=self.start, args=())
-        #self.ThreadForNewConnections.start()
-
-        self.running=True
-
     def start(self):
-        print(f'Asteapta conexiuni (oprire server cu Ctrl‐C) pe adresa ({self.mDNS_address},{self.mDNS_port})')
+        #print(f'Asteapta conexiuni (oprire server cu Ctrl‐C) pe adresa ({self.mDNS_address},{self.mDNS_port})')
+        try:
+            receive_thread = threading.Thread(target=self.receive_server)
+            receive_thread.start()
+            print("Se porneste thread ul pentru receive")
+        except:
+            print("Eroare la pornirea thread‐ului")
 
     def registerDevice(self,name):
         try:
+            print(f"\nSe va inregistra un nou device cu numele {name}")
             # adaugam un nou dispozitiv
             newDevice = Device(str(name))
-            newDevice.thread = threading.Thread(target=self.handleDevice)  # functia va receptiona mesajele de la Device
+            #newDevice.thread = threading.Thread(target=self.handleDevice)  # functia va receptiona mesajele de la Device
             self.clients.append(newDevice)
-            print("mema")
             self.local_network.register_device(newDevice)
 
             # apasarea tastelor Ctrl‐C se iese din blucla while 1
         except KeyboardInterrupt:
             print("Bye bye")
 
-    def handleDevice(conn, addr):
-        #trebuie editata dupa nevoile noastre+pachete de date
-        while 1:
-            # Asteapta date, buffer de 1024 octeti (apel blocant)
-            data = conn.recv(1024)
-            # Daca functia recv returneaza None, clientul a inchis conexiunea
-            if not data:
-                break
-            print(addr, ' a trimis: ', data)
-            # Trimite inapoi datele receptionate
-            conn.sendall(bytes(str(addr) + ' a trimis ' + str(data), encoding="ascii"))
-        print("Clientul ", addr, " a inchis conexiunea")
-        conn.close()
+    def receive_server(self):
+        msgFromServer = "Hello UDP Client"
+        bytesToSend = str.encode(msgFromServer)
+        bufferSize=1024
+        while True:
+            bytesAddressPair = self.server.recvfrom(bufferSize)
+
+            message = bytesAddressPair[0]
+
+            address = bytesAddressPair[1]
+
+            clientMsg = "Message from Client:{}".format(message)
+            clientIP = "Client IP Address:{}".format(address)
+
+            print(clientMsg)
+            print(clientIP)
+
+            # Sending a reply to client
+
+            self.server.sendto(bytesToSend, address)
 
     def set_local_network(self,network):
         self.local_network=network
