9892f9c6066086987eb00bc5f4fc9249fa84bd4b
Gabriela Brezeanu <91419044+30Gabriela@users.noreply.github.com>
2022-01-07 20:08:12 +0200

    Device

--- Device.py
+++ Device.py
@@ -1,16 +1,17 @@
 import socket
 import struct
 import threading
+import time
 
 import DNS_Question
 import Header_DNS_packet
 
-serverAddressPort = ("127.0.0.1", 20002)
+server_address='127.0.0.1'
 multicast_group=('224.3.29.71',5353)
 class Device:
     def __init__(self,domain_name):
         self.router_address=''
-        self.subnet_mask=''
+        self.subnet_mask='255.255.255.0'
         self.port=0
         self.domain_name=domain_name
         self.services=[] #o lista in care se vor inregistra serviciile oferite
@@ -19,21 +20,14 @@ class Device:
         self.thread=None
 
         try:
-            self.server_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-            self.server_receive.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            self.server_receive.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            #print(self.server_send)
-            ttl = struct.pack('b', 1)
-            self.server_receive.bind(('',5353))
-
-
+            self.server_receive = self.multicast_socket()
+            self.server_receive.bind(('',self.mDNS_port))
             group = socket.inet_aton('224.3.29.71')
             mreq = struct.pack('4sL', group, socket.INADDR_ANY)
             self.server_receive.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-            print("S-a creat un socket pentru device........")
+            print("S-a creat un socket_receive pentru device........")
         except BaseException as error:
             print("Error " + error)
-            raise
 
         try:
             receive_thread = threading.Thread(target=self.device_receive)
@@ -42,35 +36,54 @@ class Device:
         except:
             print("Eroare la pornirea thread‐ului")
 
-    def multicast_socket(self,port):
+    def multicast_socket(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #ttl=struct.pack('b',6)
-        #server.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-        #loop = struct.pack(b'B', 1)
-        #server.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, loop)
-        server.bind(('', port))
+        #server.bind(('127.0.0.1', port))
         return server
 
     def device_receive(self):
         msgFromClient = "Hello UDP Server"
-        msgFromClient=DNS_Question.DNS_Question(DNS_Question.DNS_Question.TYPE_A,
-                                                DNS_Question.DNS_Question.QCLASS_INTERNET,"myPersonalPc.local").get_dns_question()
-        #bytesToSend = str.encode(msgFromClient)
+        #msgFromClient=DNS_Question.DNS_Question(DNS_Question.DNS_Question.TYPE_A,
+        #                                        DNS_Question.DNS_Question.QCLASS_INTERNET,"myPersonalPc.local").get_dns_question()
+        bytesToSend = str.encode(msgFromClient)
         #bytesToSend=msgFromClient
         #self.server_send.sendto(bytesToSend, multicast_group)
         while 1:
-            msgFromServer = self.server_receive.recvfrom(1024)
-            msg = f"Device-ul {self.router_address} a primit de la server : {msgFromServer[0]}"
-            print(msg)
-            msgFromClient = "akg"
-            bytesToSend = str.encode(msgFromClient)
-            #self.server_receive.sendto(bytesToSend, multicast_group)
+            try:
+                msgFromServer = self.server_receive.recvfrom(1024)
+                msg = f"Device-ul {self.router_address} a primit de la server : {msgFromServer[0]}"
+                print(msg)
+                time.sleep(1)
+                self.device_send("Am primit salutul")
+            except Exception as err:
+                print(err)
+
+    def device_send(self, msg):
+        try:
+            self.server_send.sendto(str.encode(msg),('127.0.0.1',self.port))
+        except Exception as err:
+            print(err)
+        #self.server_send.sendto(str.encode(msg), (self.router_address, self.port))
 
     def set_router_address(self,IP):
         self.router_address=IP
 
+    def set_port(self,port):
+        self.port=port
+        self.server_send = self.multicast_socket()
+        try:
+            self.server_send.bind((server_address,self.port))
+        except Exception as err:
+            print(err)
+            print("device..........")
+
+    def get_router_address(self):
+        return self.router_address
+
+    def get_port(self):
+        return self.port
 
     def close(self):
         #inchide conexiunea
